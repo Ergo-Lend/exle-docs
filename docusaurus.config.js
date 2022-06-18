f6f9fd1650b4f52d0377cae0813561688d7901fc
Keith Lim <keithlimweiluen@gmail.com>
2022-06-18 15:49:35 -0700

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Exle Dev Docs',
   tagline: 'Development in Exle',
   url: 'https://ergo-lend.github.io',
-  baseUrl: '/deco-docs/',
+  baseUrl: '/exle-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ergo-lend', // Usually your GitHub org/user name.
   projectName: 'exle-docs', // Usually your repo name.
-  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
